0abe691290074f36133164e5704ab89bfdfcb663
Jin Cheng <jincheng411@gmail.com>
2021-09-30 17:28:33 -0400

    completed product styles query

--- models/styles.js
+++ models/styles.js
@@ -4,23 +4,39 @@ module.exports = {
   getStylesByProductId: (productId, cb) => {
     db.query(`
     SELECT  p.id AS product_id,
-            ARRAY_AGG(JSON_BUILD_OBJECT(
-              'style_id', s.id,
-              'name', s.name,
-              'original_price', s.original_price,
-              'sale_price', s.sale_price,
-              'default?', s.default_style,
-              'photos', (SELECT ARRAY_AGG(JSON_BUILD_OBJECT(
-                          'thumbnail_url', photos.thumbnail_url,
-                          'url', photos.url
-                        ))
-                        FROM photos
-                        JOIN styles
-                        ON styles.id = photos.styleId
-                        WHERE styles.id = p.id
-                        GROUP BY styles.id)
+            ARRAY_AGG(JSON_BUILD_OBJECT
+              (
+                'style_id', s.id,
+                'name', s.name,
+                'original_price', s.original_price,
+                'sale_price', s.sale_price,
+                'default?', s.default_style,
+                'photos', (SELECT ARRAY_AGG(JSON_BUILD_OBJECT(
+                            'thumbnail_url', photos.thumbnail_url,
+                            'url', photos.url
+                          ))
+                          FROM photos
+                          JOIN styles
+                          ON styles.id = photos.styleId
+                          WHERE styles.id = p.id
+                          GROUP BY styles.id),
+                'skus', (SELECT JSON_OBJECT_AGG(
+                                sk.id, (SELECT JSON_BUILD_OBJECT(
+                                                      'size', skus.size,
+                                                      'quantity', skus.quantity
+                                                    )
+                                          FROM skus
+                                          WHERE skus.id = sk.id
+                                        )
+                                )
+                          FROM skus AS sk
+                          JOIN styles
+                          ON styles.id = sk.styleId
+                          WHERE styles.id = s.id
+                          GROUP BY styles.id
                         )
-              ) AS results
+              )
+            ) AS results
     FROM styles AS s
     JOIN products AS p
     ON p.id = s.product_id

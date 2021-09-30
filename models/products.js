f6eb78029418bb7d22e924fc793211eda9d4fc72
Jin Cheng <jincheng411@gmail.com>
2021-09-30 00:12:26 -0400

    added page and count query functionality

--- models/products.js
+++ models/products.js
@@ -1,7 +1,12 @@
 const db = require('../db');
 module.exports = {
-  getAllProducts: (cb) => {
-    db.query('SELECT * FROM products LIMIT 100;', (err, data) => {
+  getAllProducts: (page, count, cb) => {
+    const queryString = `
+    SELECT *
+    FROM products
+    LIMIT ${count}
+    OFFSET ${count * (page - 1)};`;
+    db.query(queryString, (err, data) => {
       cb(err, data.rows);
     })
   },
@@ -13,7 +18,7 @@ module.exports = {
     JOIN features AS f
     ON p.id = f.product_id
     WHERE p.id = ${id}
-    GROUP BY p.id
+    GROUP BY p.id;
     `;
     db.query(queryString, (err, data) => {
       cb(err,data.rows);
@@ -21,14 +26,3 @@ module.exports = {
   }
 }
 
-// SELECT p.id, p.name, p.slogan,p.description, p.category, p.default_price,
-//       ARRAY_AGG(
-//         SELECT *
-//         FROM features AS f
-//         WHERE f.id = 1
-//       ) AS features
-//     FROM products AS p
-//     JOIN features AS f
-//     ON p.id = f.product_id
-//     WHERE p.id = ${id}
-//     GROUP BY p.id

d27c3b7dbe2c5ee8128a334d4db5fd71a0cc4a8f
Jin Cheng <jincheng411@gmail.com>
2021-09-30 12:22:46 -0400

    completed styles and photos query

--- models/styles.js
+++ models/styles.js
@@ -3,8 +3,24 @@ const db = require('../db');
 module.exports = {
   getStylesByProductId: (productId, cb) => {
     db.query(`
-    SELECT  p.id AS product_id, ARRAY_AGG(JSON_BUILD_OBJECT('style_id', s.id, 'name', s.name, 'original_price', s.original_price, 'sale_price', s.sale_price,
-           'default?', s.default_style)) AS results
+    SELECT  p.id AS product_id,
+            ARRAY_AGG(JSON_BUILD_OBJECT(
+              'style_id', s.id,
+              'name', s.name,
+              'original_price', s.original_price,
+              'sale_price', s.sale_price,
+              'default?', s.default_style,
+              'photos', (SELECT ARRAY_AGG(JSON_BUILD_OBJECT(
+                          'thumbnail_url', photos.thumbnail_url,
+                          'url', photos.url
+                        ))
+                        FROM photos
+                        JOIN styles
+                        ON styles.id = photos.styleId
+                        WHERE styles.id = p.id
+                        GROUP BY styles.id)
+                        )
+              ) AS results
     FROM styles AS s
     JOIN products AS p
     ON p.id = s.product_id

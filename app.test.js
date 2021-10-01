d3b74e852358a3c62ead12108e74e384f543bd31
Jin Cheng <jincheng411@gmail.com>
2021-10-01 19:23:03 -0400

    style testing

--- app.test.js
+++ app.test.js
@@ -19,6 +19,39 @@ describe('get /products', () => {
     const response = await request(app).get('/api/products?count=8&page=2').send({});
     expect(response.body.length).toEqual(8)
     expect(response.body[0].id).toEqual(9)
-
   })
 })
+
+describe('get /products/:id', () => {
+  test('should return a 200 status code', async () => {
+    const response = await request(app).get('/api/products/1').send({});
+    expect(response.statusCode).toBe(200)
+  })
+  test('should return an object', async () => {
+    const response = await request(app).get('/api/products/1').send({});
+    expect(response.body).toBeInstanceOf(Object)
+  })
+  test('should return proper result when id=1 ', async () => {
+    const response = await request(app).get('/api/products/1').send({});
+    expect(response.body[0].id).toEqual(1)
+  })
+  test('should return proper result when id=3 ', async () => {
+    const response = await request(app).get('/api/products/3').send({});
+    expect(response.body[0].id).toEqual(3)
+  })
+})
+
+// describe('get /products/:id/styles', () => {
+//   test('should return a 200 status code', async () => {
+//     const response = await request(app).get('/api/products/1/styles').send({});
+//     expect(response.statusCode).toBe(200)
+//   })
+//   test('should return an Array', async () => {
+//     const response = await request(app).get('/api/products/1/styles').send({});
+//     expect(response.body).toBeInstanceOf(Array)
+//   })
+//   test('should return proper result when id=1 ', async () => {
+//     const response = await request(app).get('/api/products/1/styles').send({});
+//     expect(response.body[0].id).toEqual(1)
+//   })
+// })
